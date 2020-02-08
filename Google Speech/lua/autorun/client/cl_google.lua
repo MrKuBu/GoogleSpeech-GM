a3c826ba9575dd51acd5bdcaf0b9a3b7fb0277b3
MrKuBu <mrkubu122333@gmail.com>
2020-02-08 14:37:02 +0300

    Change var tts
    Add speech change from Google or Yandex

--- Google Speech/lua/autorun/client/cl_google.lua
+++ Google Speech/lua/autorun/client/cl_google.lua
@@ -7,6 +7,7 @@ local net_Start, net_WriteString, net_SendToServer = net.Start, net.WriteString,
 local RunConsoleCommand, net_Receive = RunConsoleCommand, net.Receive
 
 local GoogleBool = CreateClientConVar("cl_google_enabled", "1", true)
+local IsGoogle = CreateClientConVar("cl_google_or_yandex", "1", true)
 local GoogleInWorld = CreateClientConVar("cl_google_enabled_3d", "0", true)
 local GoogleVoice = CreateClientConVar("cl_google_voice", "oksana", true)
 
@@ -23,7 +24,11 @@ hook_Add("OnPlayerChat", "OnPlayerChat_Google", function(ply, text, teamchat, de
 	if !IsValid(ply) or !GoogleBool:GetBool() then return end
 	
 	if goospeech:HasValue(ply) then
-		local encode = string_format("http://tts.voicetech.yandex.net/tts?speaker=%s&text=%s", goospeech:GetVoice(ply), httpUrlEncode(text))
+		if IsGoogle:GetBool() then
+			local encode = string.format("http://translate.google.com/translate_tts?ie=UTF-8&tl=ru&client=tw-ob&q=%s", httpUrlEncode(text))
+		else
+			local encode = string_format("http://tts.voicetech.yandex.net/tts?speaker=%s&text=%s", goospeech:GetVoice(ply), httpUrlEncode(text))	
+		end
 		local flag = "mono"
 		if GoogleInWorld:GetBool() then
 			flag = "3D"
